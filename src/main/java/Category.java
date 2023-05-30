e7b9245c17f8325bfe9b02b2c43dc8f185f324f6
hposyniak <hposyniak@gmail.com>
2023-05-30 18:59:17 +0200

    .

---
+++ src/main/java/Category.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+public class Category {
+
+    private String name;
+    private LinkedList questions = new LinkedList();
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addQuestion(int i) {
+
+        questions.addLast(getName() + " Question " + i);
+
+    }
+    public void removeQuestion(){
+
+        System.out.println(questions.removeFirst());
+
+    }
+}
